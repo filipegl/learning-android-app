98d12109afade5de908c40425d7d3f215b7b0c80
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2019-01-27 18:15:58 -0300

    Classificando se está sorrindo, sério, piscando, olhos abertos ou alguma combinação destes

--- AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
+++ AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -11,6 +11,9 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
 public class Emojifier {
+
+
+
     static void detectFaces(Context context, Bitmap bitmap){
         FaceDetector detector = new FaceDetector.Builder(context)
                 .setTrackingEnabled(false)
@@ -26,6 +29,77 @@ public class Emojifier {
         }
         Log.d("FACES", "Number of faces = " + numFaces);
 
+        for (int i = 0; i < faces.size(); i++){
+            whichEmoji(faces.valueAt(i));
+        }
+
         detector.release();
+    }
+
+    static Emoji whichEmoji(Face face){
+
+        boolean sorrindo = false;
+        boolean olho_esquerdo_aberto = false;
+        boolean olho_direito_aberto = false;
+        Emoji emoji = Emoji.SORRINDO;
+
+        if (face.getIsSmilingProbability() >= 0.5) {
+            sorrindo = true;
+        }
+        if (face.getIsRightEyeOpenProbability() >= 0.5){
+            olho_direito_aberto = true;
+        }
+        if (face.getIsLeftEyeOpenProbability() >= 0.5){
+            olho_esquerdo_aberto = true;
+        }
+
+        if (sorrindo){
+            if (olho_direito_aberto && olho_esquerdo_aberto){
+                emoji = Emoji.SORRINDO;
+            } else {
+                if (olho_esquerdo_aberto || olho_direito_aberto){
+                    if (olho_direito_aberto){
+                        //ESQUERDO FECHADO
+                        emoji = Emoji.PISCADA_ESQUERDA;
+                    } else {
+                        //ESQUERDO ABERTO
+                        emoji = Emoji.PISCADA_DIREITA;
+                    }
+                } else {
+                    emoji = Emoji.OLHOS_FECHADOS_SORRINDO;
+                }
+            }
+        } else {
+            if (olho_direito_aberto && olho_esquerdo_aberto){
+                emoji = Emoji.SERIO;
+            } else {
+                if (olho_esquerdo_aberto || olho_direito_aberto){
+                    if (olho_direito_aberto){
+                        //ESQUERDO FECHADO
+                        emoji = Emoji.PISCADA_ESQUERDA_SERIO;
+                    } else {
+                        //ESQUERDO ABERTO
+                        emoji = Emoji.PISCADA_DIREITA_SERIO;
+                    }
+                } else {
+                    emoji = Emoji.OLHOS_FECHADOS_SERIO;
+                }
+            }
+        }
+
+        Log.d("EMOJI", "Face" + face.getId() + " - EMOJI: " + emoji.name());
+
+        return emoji;
+    }
+
+    public enum Emoji {
+        SORRINDO,
+        SERIO,
+        PISCADA_DIREITA,
+        PISCADA_DIREITA_SERIO,
+        PISCADA_ESQUERDA,
+        PISCADA_ESQUERDA_SERIO,
+        OLHOS_FECHADOS_SORRINDO,
+        OLHOS_FECHADOS_SERIO;
     }
 }
