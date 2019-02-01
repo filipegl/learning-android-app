9a37397e3d301e70c8e85d4789537a56bd0d86b4
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2019-02-01 17:34:45 -0300

    Sobresrevendo emojis nos rostos

--- AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
+++ AdvancedAndroid_Emojify/app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -2,6 +2,8 @@ package com.example.android.emojify;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.Log;
 import android.util.SparseArray;
 import android.widget.Toast;
@@ -14,7 +16,7 @@ public class Emojifier {
 
 
 
-    static void detectFaces(Context context, Bitmap bitmap){
+    static Bitmap detectFacesAndOverlayEmoji(Context context, Bitmap bitmap){
         FaceDetector detector = new FaceDetector.Builder(context)
                 .setTrackingEnabled(false)
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
@@ -29,29 +31,49 @@ public class Emojifier {
         }
         Log.d("FACES", "Number of faces = " + numFaces);
 
+        Bitmap resultBitmap = bitmap;
         for (int i = 0; i < faces.size(); i++){
-            whichEmoji(faces.valueAt(i));
+
+            Bitmap emojiBitmap = null;
+            switch (whichEmoji(faces.valueAt(i))){
+                case SERIO:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.frown);
+                    break;
+                case SORRINDO:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.smile);
+                    break;
+                case OLHOS_FECHADOS_SERIO:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.closed_frown);
+                    break;
+                case OLHOS_FECHADOS_SORRINDO:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.closed_smile);
+                    break;
+                case PISCADA_DIREITA:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.rightwink);
+                    break;
+                case PISCADA_ESQUERDA:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.leftwink);
+                    break;
+                case PISCADA_DIREITA_SERIO:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.rightwinkfrown);
+                    break;
+                case PISCADA_ESQUERDA_SERIO:
+                    emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.leftwinkfrown);
+                    break;
+            }
+            resultBitmap = addBitmapToFace(resultBitmap, emojiBitmap, faces.valueAt(i));
         }
 
         detector.release();
+        return resultBitmap;
     }
 
     static Emoji whichEmoji(Face face){
 
-        boolean sorrindo = false;
-        boolean olho_esquerdo_aberto = false;
-        boolean olho_direito_aberto = false;
+        boolean sorrindo = face.getIsSmilingProbability() >= 0.15;
+        boolean olho_esquerdo_aberto = face.getIsLeftEyeOpenProbability() >= 0.2;
+        boolean olho_direito_aberto = face.getIsRightEyeOpenProbability() >= 0.2;
         Emoji emoji = Emoji.SORRINDO;
-
-        if (face.getIsSmilingProbability() >= 0.5) {
-            sorrindo = true;
-        }
-        if (face.getIsRightEyeOpenProbability() >= 0.5){
-            olho_direito_aberto = true;
-        }
-        if (face.getIsLeftEyeOpenProbability() >= 0.5){
-            olho_esquerdo_aberto = true;
-        }
 
         if (sorrindo){
             if (olho_direito_aberto && olho_esquerdo_aberto){
@@ -88,8 +110,48 @@ public class Emojifier {
         }
 
         Log.d("EMOJI", "Face" + face.getId() + " - EMOJI: " + emoji.name());
+        Log.d("EMOJI", "Esquerdo aberto: " +face.getIsLeftEyeOpenProbability() + "Direito aberto: " +face.getIsRightEyeOpenProbability() + "Sorrindo: " + face.getIsSmilingProbability());
 
         return emoji;
+    }
+    /**
+     * Combines the original picture with the emoji bitmaps
+     *
+     * @param backgroundBitmap The original picture
+     * @param emojiBitmap      The chosen emoji
+     * @param face             The detected face
+     * @return The final bitmap, including the emojis over the faces
+     */
+    private static Bitmap addBitmapToFace(Bitmap backgroundBitmap, Bitmap emojiBitmap, Face face) {
+
+        // Initialize the results bitmap to be a mutable copy of the original image
+        Bitmap resultBitmap = Bitmap.createBitmap(backgroundBitmap.getWidth(),
+                backgroundBitmap.getHeight(), backgroundBitmap.getConfig());
+
+        // Scale the emoji so it looks better on the face
+        float scaleFactor = 0.9f;
+
+        // Determine the size of the emoji to match the width of the face and preserve aspect ratio
+        int newEmojiWidth = (int) (face.getWidth() * scaleFactor);
+        int newEmojiHeight = (int) (emojiBitmap.getHeight() *
+                newEmojiWidth / emojiBitmap.getWidth() * scaleFactor);
+
+
+        // Scale the emoji
+        emojiBitmap = Bitmap.createScaledBitmap(emojiBitmap, newEmojiWidth, newEmojiHeight, false);
+
+        // Determine the emoji position so it best lines up with the face
+        float emojiPositionX =
+                (face.getPosition().x + face.getWidth() / 2) - emojiBitmap.getWidth() / 2;
+        float emojiPositionY =
+                (face.getPosition().y + face.getHeight() / 2) - emojiBitmap.getHeight() / 3;
+
+        // Create the canvas and draw the bitmaps to it
+        Canvas canvas = new Canvas(resultBitmap);
+        canvas.drawBitmap(backgroundBitmap, 0, 0, null);
+        canvas.drawBitmap(emojiBitmap, emojiPositionX, emojiPositionY, null);
+
+        return resultBitmap;
     }
 
     public enum Emoji {
